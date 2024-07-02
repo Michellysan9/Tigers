8e0b858ba38ab32b279049803ca73d7f996efbb9
Maria del Mar <mariadelmarmosqueracaicedo@gmail.com>
2024-07-02 14:26:49 -0500

    angular-material

--- my-workspace/src/app/buscador/buscador.component.ts
+++ my-workspace/src/app/buscador/buscador.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buscador',
-  standalone: true,
+  standalone: true, 
   imports: [],
   templateUrl: './buscador.component.html',
   styleUrl: './buscador.component.css'
