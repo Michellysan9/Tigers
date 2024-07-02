9858a528e9dacad33b2c66adc6c4fb0bf0115a9f
Maria del Mar <mariadelmarmosqueracaicedo@gmail.com>
2024-07-02 17:31:14 -0500

    componente y pagina buscador

---
+++ my-workspace/src/app/pages/pagina-buscador/pagina-buscador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-buscador',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-buscador.component.html',
+  styleUrl: './pagina-buscador.component.css'
+})
+export class PaginaBuscadorComponent {
+
+}
