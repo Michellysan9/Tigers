408dda656c654ce900b28497da2c8bc4ffcf47fb
Maria del Mar <mariadelmarmosqueracaicedo@gmail.com>
2024-07-30 20:21:39 -0500

    audio y visualizador de pdf

--- my-workspace/src/app/pages/book-page/book-page.component.ts
+++ my-workspace/src/app/pages/book-page/book-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from '../../book';
 import { books } from '../../lista_de_libros';
 import { CommonModule } from '@angular/common';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { AudiolibroComponent } from '../../components/audiolibro/audiolibro.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-book-page',
   standalone: true,
-  imports: [CommonModule, NgxExtendedPdfViewerModule],
+  imports: [CommonModule, NgxExtendedPdfViewerModule, AudiolibroComponent],
   templateUrl: './book-page.component.html',
   styleUrl: './book-page.component.css'
 })
@@ -16,15 +18,19 @@ export class BookPageComponent {
   book: Book | undefined;
   pdfSource: string | undefined;
 
-  constructor(private route: ActivatedRoute) {}
-
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer, private router: Router) {}
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       const bookName = params.get('name');
       this.book = books.find(b => b.name === bookName)
       if (this.book) {
-        this.pdfSource = this.book.url
+        this.pdfSource = this.sanitizer.bypassSecurityTrustResourceUrl(this.book.url) as string;
       }
     })
+  }
+
+  volver(){
+    this.router.navigate(['/buscador'])
+
   }
 }
